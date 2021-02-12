d2b66a895f53594d6a7e5e5b0ca2b93d30dcba13
KoninklijkeSev <strong.m.m@mail.ru>
2021-02-13 00:55:36 +0300

    Add homework task 3.2

---
+++ src/test/java/tests/TestsCheckInfoOnPageSoftAssertions.java
@@ -0,0 +1,30 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestsCheckInfoOnPageSoftAssertions {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void checkInfoOnPageSoftAssertions () {
+
+        open("https://github.com/");
+        $("[name = q]").setValue("Selenide").pressEnter();
+        $(".repo-list a").click();
+        $(byText("Wiki")).click();
+        $("[data-filterable-for]").shouldHave(text("SoftAssertions")).click();
+        $(byText("SoftAssertions")).click();
+        $("[start = '3']").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
